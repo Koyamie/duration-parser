275658a95873fdde04ca61bb8c85792647257a0e
Koyamie <19957779+Koyamie@users.noreply.github.com>
2025-01-29 01:27:30 +0100

    feat: declare module

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
-export type Parser = (input: string) => number;
-export const durationParser: Parser;
+declare module 'duration-parser' {
+  export type Parser = (input: string) => number;
+  export const durationParser: Parser;
+}
